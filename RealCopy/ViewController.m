59887a0698950e73b7f91c220f71e7fec17a752c
Yaming <yax@microsoft.com>
2017-09-10 20:04:29 +0900

    init

--- RealCopy/ViewController.m
+++ RealCopy/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "CameraSessionView.h"
 
-@interface ViewController ()
+@interface ViewController () <CACameraSessionDelegate>
+
+@property (nonatomic, strong) CameraSessionView *cameraView;
 
 @end
 
@@ -17,8 +20,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _cameraView = [[CameraSessionView alloc] initWithFrame:self.view.frame];
+    _cameraView.delegate = self;
+    
+    [_cameraView setTopBarColor:[UIColor colorWithRed:0.97 green:0.97 blue:0.97 alpha: 0.64]];
+    [_cameraView hideFlashButton];
+    [_cameraView hideCameraToggleButton];
+    [_cameraView hideDismissButton];
+    
+    [self.view addSubview:_cameraView];
 }
 
+-(void)didCaptureImage:(UIImage *)image {
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    //[self.cameraView removeFromSuperview];
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    if (error) {
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                       message:@"Image saving fails..."
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK"
+                                                                style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * action) {}];
+        [alert addAction:defaultAction];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
